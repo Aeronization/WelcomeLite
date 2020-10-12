0439dd39a088a9ecf91bf0e0aa1bc94f0b700341
Ryan <rfriel@rezjam.com>
2020-10-12 15:27:29 -0400

    Add the JobOffer model with the basic fields outlined in the project. Will add foreign key to user later.

--- job_offers/models.py
+++ job_offers/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class JobOffer(models.Model):
+    candidate_first_name = models.CharField(max_length=255)
+    candidate_last_name = models.CharField(max_length=255)
+    candidate_email_address = models.EmailField(max_length=128, null=True)
