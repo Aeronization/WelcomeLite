ba49fb6c1c642738dd63725cb0837f3bfc9b25c5
Ryan <rfriel@rezjam.com>
2020-10-14 14:13:35 -0400

    Add guinicorn use in the Procfile for herkou. Add gunicorn to requirements. Update settings for production deployment to heroku.

--- welcome_lite/settings.py
+++ welcome_lite/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,7 +61,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'welcome_lite.urls'
