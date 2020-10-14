549d6e97128a9ccc0ec911d9a9138e950608b2e4
Ryan <rfriel@rezjam.com>
2020-10-14 12:52:16 -0400

    Add the following / to the urls.

--- job_offers/urls.py
+++ job_offers/urls.py
@@ -13,7 +13,7 @@ app_name = "job_offers"
 urlpatterns = [
     path("", JobOfferHome.as_view(), name="home"),
     path("create/", JobOfferCreate.as_view(), name="create"),
-    path("read/<int:pk>", JobOfferRead.as_view(), name="read"),
-    path("update/<int:pk>", JobOfferUpdate.as_view(), name="update"),
-    path("delete/<int:pk>", JobOfferDelete.as_view(), name="delete")
+    path("read/<int:pk>/", JobOfferRead.as_view(), name="read"),
+    path("update/<int:pk>/", JobOfferUpdate.as_view(), name="update"),
+    path("delete/<int:pk>/", JobOfferDelete.as_view(), name="delete")
 ]
